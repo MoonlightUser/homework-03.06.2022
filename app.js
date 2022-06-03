9d316eb2bdb3489088ee76e845c0e046c6d3dcc4
MoonlightUser <tymurmarenych@gmail.com>
2022-06-03 21:44:29 +0100

    first commit

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ const nameOfUser = prompt("Put here your name :>");
 let ageOfUser = prompt("Put here your age too pls :>");
 alert(`${nameOfUser}, ur age is ${ageOfUser}`)
 ageOfUser = 23;
+// first commit
